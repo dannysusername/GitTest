812cdc269f4b2d28f4218669925db02a604fc588
Daniel Ibarra <dannibar201@gmail.com>
2026-02-14 11:42:56 -0500

    added tech

---
+++ newtech.java
@@ -0,0 +1,8 @@
+public class newtech {
+
+    public void newTech() {
+        System.out.println("Stella Montis trigger nade dupe");
+        System.out.println("Rubber ducks dupe, 5mil per raid");
+    }
+
+}
--- test.java
+++ test.java
@@ -4,10 +4,14 @@ import java.util.*;
 public class test {
     public static void main(String[] args) {
         System.out.println("test");
+        newtech stellaM = new newtech();
+        stellaM.newTech();
+        
     }
 
     public int add(int a, int b) {
         return a + b;
     }
+
 
 }
