5f8d1093a83c494410e3365f1c466c2afc6bb369
Daniel Ibarra <dannibar201@gmail.com>
2026-02-14 14:04:36 -0500

    Added trigger nade multiplier

--- newtech.java
+++ newtech.java
@@ -1,8 +1,18 @@
 public class newtech {
 
+    int trigNades = 1;
+
     public void newTech() {
+
         System.out.println("Stella Montis trigger nade dupe");
+        dupeTriggerNades(trigNades);
         System.out.println("Rubber ducks dupe, 5mil per raid");
+    }
+
+    public int dupeTriggerNades(int x) {
+        trigNades = trigNades * 2;
+        return trigNades;
+
     }
 
 }
