eccb3dceab3a2b0090da583c31c5358c1d7db9c9
Daniel Ibarra <dannibar201@gmail.com>
2026-02-14 16:13:20 -0500

    test

--- test.java
+++ test.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("testt");
         newtech stellaM = new newtech();
         stellaM.newTech();
         
