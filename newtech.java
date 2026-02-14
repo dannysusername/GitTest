fbfdda233c303310001abc4a11c1c409f4100eef
Daniel Ibarra <dannibar201@gmail.com>
2026-02-14 14:21:14 -0500

    Rubber duck dupe

--- newtech.java
+++ newtech.java
@@ -1,17 +1,25 @@
 public class newtech {
 
-    int trigNades = 1;
+    int trigNades = 2;
+    int rubberDucks = 2;
 
     public void newTech() {
 
         System.out.println("Stella Montis trigger nade dupe");
         dupeTriggerNades(trigNades);
         System.out.println("Rubber ducks dupe, 5mil per raid");
+        dupeRubberDucks(rubberDucks);
     }
 
     public int dupeTriggerNades(int x) {
-        trigNades = trigNades * 2;
+        trigNades = trigNades + 1;
         return trigNades;
+
+    }
+
+    public int dupeRubberDucks(int x) {
+        rubberDucks = rubberDucks + 1;
+        return rubberDucks;
 
     }
 
